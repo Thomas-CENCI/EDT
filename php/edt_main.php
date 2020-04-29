2a71186b4aaee89422dee311fef8f3b313f7f355
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-04-29 12:13:22 +0200

    affichage emploi du temps page0

--- php/edt_main.php
+++ php/edt_main.php
@@ -21,7 +21,7 @@
 	<body>
 
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  		<a class="navbar-brand" href="?/page=0">EDT</a>
+	  		<a class="navbar-brand" href="?page=0">EDT</a>
 	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    		<span class="navbar-toggler-icon"></span>
 	  		</button>
@@ -29,18 +29,18 @@
 	  		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		   		<ul class="navbar-nav mr-auto">
 		      		<li class="nav-item active">
-		        		<a class="nav-link" href="?/page=0">Emploi du temps <span class="sr-only">(current)</span></a>
+		        		<a class="nav-link" href="?page=0">Emploi du temps <span class="sr-only">(current)</span></a>
 		     		</li>
 		     		<?php if($statut == "admin"): ?>
 			      		<li class="nav-item">
-			        		<a class="nav-link" href="?/page=1">Gestion cours</a> <!-- Permet à l'admin d'ajouter, de supprimer et de modifier des cours -->
+			        		<a class="nav-link" href="?page=1">Gestion cours</a> <!-- Permet à l'admin d'ajouter, de supprimer et de modifier des cours -->
 			      		</li>
 			      		<li class="nav-item">
-			        		<a class="nav-link" href="?/page=2">Demandes de cours</a> <!-- affiche toutes les demandes à traiter -->
+			        		<a class="nav-link" href="?page=2">Demandes de cours</a> <!-- affiche toutes les demandes à traiter -->
 			      		</li>
 			      	<?php elseif($statut == "enseigant"): ?>
 			      		<li class="nav-item">
-			        		<a class="nav-link" href="?/page=3">Demander des cours</a> <!-- Demander à l'damin d'ajouter, de supprimer ou de modifier des cours  -->
+			        		<a class="nav-link" href="?page=3">Demander des cours</a> <!-- Demander à l'damin d'ajouter, de supprimer ou de modifier des cours  -->
 			      		</li>
 			      	<?php endif; ?>
 		    	</ul>
--- php/page0.php
+++ php/page0.php
@@ -1,4 +1,91 @@
-<head>
-	<meta name=description content="Affichage emploie du temps - ALL">
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-</head>
+<style>
+<?php 
+include "bootstrap/css/bootstrap.min.css";
+include 'EDT.css'; ?>
+</style>
+  
+
+<div class="row">
+
+  <div class="col-lg-6">
+    <h1>EDT</span></h1>
+  </div>
+
+</div>
+
+<div id="menu">
+
+</div>
+
+<div id="contenu">
+<style type="text/css">
+caption /* Titre du tableau */
+{
+   margin: auto; /* Centre le titre du tableau */
+   font-family: Arial, Times, "Times New Roman", serif;
+   font-weight: bold;
+   font-size: 1.2em;
+   color: #009900;
+   margin-bottom: 20px;
+}
+
+table /* Le tableau en lui-même */
+{
+   margin: auto;
+   border: 4px outset green;
+   border-collapse: collapse;
+   width:100%;
+}
+th /* Les cellules d'en-tête */
+{
+   background-color: #006600;
+   color: white;
+   font-size: 1.1em;
+   font-family: Arial, "Arial Black", Times, "Times New Roman", serif;
+   border:1px solid red;
+}
+
+td /* Les cellules normales */
+{
+   border: 1px solid black;
+   font-family: "Comic Sans MS", "Trebuchet MS", Times, "Times New Roman", serif;
+   text-align: center;
+   padding: 5px;
+}
+td.time
+{
+    width:5%;
+}
+</style>
+<table>
+	<?php
+	    $jour = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi");
+	    $rdv["Lundi"]["9:30"] = "Maths";
+	    echo "<tr><th>Heure</th>";
+	    for($x = 1; $x < 6; $x++)
+	        echo "<th>".$jour[$x]."</th>";
+	    echo "</tr>";
+	    for($j = 7; $j < 21; $j += 0.25) {
+	        echo "<tr>";
+	        for($i = 0; $i < 5; $i++) {
+	            if($i == 0) {
+	                $heure = str_replace(".25", ":15", $j);
+	                $heure = str_replace(".5", ":30", $j); /* Ne fonctionne pas */
+	                $heure = str_replace(".75", ":45", $j);
+	                echo "<td class=\"time\">".$heure."</td>";
+	            }
+	            echo "<td>";
+	            if(isset($rdv[$jour[$i+1]][$heure])) {
+	                echo $rdv[$jour[$i+1]][$heure];
+	            }
+	            echo "</td>";
+	        }
+	        echo "</tr>";
+	    }
+	?>
+</table>
+
+</div>
+
+</div>
+		  
