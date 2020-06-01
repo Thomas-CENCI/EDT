c5f08ff1a8563f0c77b92134d562ac5703830eb3
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-06-01 18:09:31 +0200

    debut ajout creation évenement

--- php/page1.php
+++ php/page1.php
@@ -27,8 +27,75 @@ if(isset($_POST['refuser_bouton'])){ // quand on refuse une demande
 
 }
 
-if(isset($_POST['accepter_bouton'])){
+if(isset($_POST['accepter_creation_bouton'])){
+  $link = mysqli_connect($hostname, $username, $password, $db);
+    
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
 
+    $SUMMARY = $_POST[champs_2];
+    $DESCRIPTION = $_POST[champs_3];
+    $DTSTART = $_POST[champs_4];
+    $DTEND = $_POST[champs_5];
+    $ENSEIGNANT = $_POST[champs_6];
+    $LOCATION = $_POST[champs_7];
+    $GROUPE = $_POST[champs_8];
+
+
+    $requete_verifie_compatibilite_sql = "SELECT id FROM events WHERE (DTSTART<='$DTEND' and '$DTSTART'<= DTEND) OR ('$DTSTART'<=DTEND and DTSTART<= '$DTEND')"; //verifie si les dates ne se chevauchent pas
+    $result_verifie_compatibilite = mysqli_query($link, $requete_verifie_compatibilite_sql);
+
+    $nb_rows = mysqli_num_rows($result_verifie_compatibilite); //on regarde si le nombre de rows est égale à 0 (ça veut dire que l'évènement que l'on souhaite ajouter ne va pas se chevaucher sur un autre)
+    
+    /* free result set */
+    mysqli_free_result($result_verifie_compatibilite);
+
+    /* close connection */
+    mysqli_close($link);
+
+    if($nb_rows ==0){ //on regarde si le nombre de rows est égale à 0 (ça veut dire que l'évènement que l'on souhaite ajouter ne va pas se chevaucher sur un autre)
+        $link = mysqli_connect($hostname, $username, $password, $db);
+         /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        }
+        $requete_ajout_evenement_sql = "INSERT INTO events(DTSTART, DTEND, SUMMARY, LOCATION, DESCRIPTION) VALUES ('$DTSTART', '$DTEND', '$SUMMARY', '$LOCATION', '$DESCRIPTION'); INSERT INTO events2salle(identifiantEvents, identifiantSalle) SELECT LAST_INSERT_ID(), salle.identifiant FROM salle WHERE salle.nom LIKE '$LOCATION';"; //on ajoute l'évènement dans les 2 tables
+
+        $result_ajout_evenement = mysqli_query($link, $requete_ajout_evenement_sql);
+
+         /* free result set */
+        mysqli_free_result($result_verifie_compatibilite);
+
+        /* close connection */
+        mysqli_close($link);
+
+        // on supprime la demande d'évènement que l'on vient de traiter
+        $link = mysqli_connect($hostname, $username, $password, $db);
+        
+        /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        }
+        
+        $id_row = $_POST['id_row'];
+        $requete_refuser_sql = "DELETE FROM modification WHERE identifiant = $id_row";
+        $result_refus = mysqli_query($link, $requete_refuser_sql);
+
+       /* free result set */
+        mysqli_free_result($result_refus);
+      
+      /* close connection */
+        mysqli_close($link);
+        echo "<div class=\"alert alert-success\" role=\"alert\">Demande accepter.</div>";
+    }else{
+        echo "<div class=\"alert alert-danger\" role=\"alert\">Demande impossible.</div>";
+
+    }
 }
 
 ?>
@@ -51,10 +118,10 @@ if(isset($_POST['accepter_bouton'])){
   }
 
   // requete affichage modification évènement
-    $requete_modification_sql = "SELECT modification.identifiant, utilisateur.identifiant, events.SUMMARY, events.DTSTART, events.DTEND, events.LOCATION, events.description, modification.salleEvent, modification.dateEventD, modification.dateEventF FROM utilisateur, events,modification WHERE (modification.identifiantUtilisateur LIKE utilisateur.identifiant) and(events.id = modification.identifiantCours) and modification.type = 2
-    ";
+    $requete_modification_sql = "SELECT modification.identifiant, utilisateur.identifiant, events.SUMMARY, events.DTSTART, events.DTEND, events.LOCATION, events.description, modification.salleEvent, modification.dateEventD, modification.dateEventF FROM utilisateur, events,modification WHERE (modification.identifiantUtilisateur LIKE utilisateur.identifiant) and(events.id = modification.identifiantCours) and modification.type = 2";
     $result_modification = mysqli_query($link, $requete_modification_sql);
     $nb_champs1 = mysqli_field_count($link); 
+    echo "nb :".$nb_champs1;
 
   ?>
 <div class="container">
@@ -116,7 +183,7 @@ if(isset($_POST['accepter_bouton'])){
   }
 
 
-  $requete_creation_sql = "SELECT modification.identifiant,utilisateur.identifiant, modification.nomEvent, modification.descriptionEvent, modification.dateEvent, modification.enseignantEvent, modification.salleEvent, modification.heureDebut, modification.heureFin, modification.groupeEvent FROM utilisateur, modification WHERE (modification.identifiantUtilisateur LIKE utilisateur.identifiant) and modification.type = 1";
+  $requete_creation_sql = "SELECT modification.identifiant,utilisateur.identifiant, modification.nomEvent, modification.descriptionEvent, modification.dateEventD, modification.dateEventF, modification.enseignantEvent, modification.salleEvent, modification.groupeEvent FROM utilisateur, modification WHERE (modification.identifiantUtilisateur LIKE utilisateur.identifiant) and modification.type = 1";
   $result_creation = mysqli_query($link, $requete_creation_sql);
   $nb_champs2 = mysqli_field_count($link);
 
@@ -132,11 +199,10 @@ if(isset($_POST['accepter_bouton'])){
             <th scope="col">Auteur</th>
             <th scope="col">Nom</th>
             <th scope="col">Description</th>
-            <th scope="col">Date</th>
+            <th scope="col">Date début</th>
+            <th scope="col">Date fin</th>
             <th scope="col">Enseignant </th>
             <th scope="col">Salle</th>
-            <th scope="col">Heure début</th>
-            <th scope="col">Heure fin</th>
             <th scope="col">Groupe</th>
             <th scope="col">Choix</th>
           </tr>
@@ -146,11 +212,12 @@ if(isset($_POST['accepter_bouton'])){
          <form action="" method="POST">
             <input type="hidden" name="id_row" value=<?="$row[0]";?>>
             <tr>
-                <?php for($i=1; $i<$nb_champs2; $i++ ){
+                <?php for($i=1; $i<$nb_champs2; $i++){
+                    echo "<input type=\"hidden\" name=\"champs_$i\" value=\"$row[$i]\">";
                     echo "<td>$row[$i]</td>";
                 } ?>
                 <td>
-                  <input type="submit" name="accepter_bouton" class="btn btn-primary" value="accepter">
+                  <input type="submit" name="accepter_creation_bouton" class="btn btn-primary" value="accepter">
                   <input type="submit" name="refuser_bouton" class="btn btn-danger" value="refuser">
                 </td>
             </tr>
