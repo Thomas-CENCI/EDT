6d5586799a22a0746eef38e443f88bdef53795bf
Thomas-CENCI <thcenci@gmail.com>
2020-06-01 11:00:27 +0200

    Type de demande

--- php/page3.php
+++ php/page3.php
@@ -1,8 +1,3 @@
-<head>
-	<meta name=description content="Demande de création d'un évennement">
-	<link href="../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-</head>
-
 <?php
 
 $hostname ="sql7.freemysqlhosting.net:3306";
@@ -11,118 +6,99 @@ $password="ItBWtR3xM5";
 $db="sql7336475";
 $conn = new mysqli($hostname, $username, $password, $db) or die('Error connecting to database');
 
-$SQL_salle = "SELECT salles.nom FROM salles";
-$requete_salle = mysqli_query($conn, $SQL_salle);
+$SQL_salle = "SELECT salle.nom FROM salle";
+$response = mysqli_query($conn, $SQL_salle);
+$requete_salle = mysqli_fetch_all($response, MYSQLI_ASSOC);
 
-// if isset $_POST("submit"){
-	// $nom
-	// $date
-	// $enseignant
-	// $salle
-	// $heureFin
-	// $heureDebut
-	// $groupe
-// }
+if(isset($_POST["submit"])){
+	$nom = $_POST["nom"];
+	$enseignant = $_POST["enseignant"];
+	$groupe = $_POST["groupe"];
+	$salle = $_POST["salle"];
+	$date = $_POST["date"];
+	$heureDebut = $_POST["hd"];
+	$heureFin = $_POST["hf"];
+
+
+	$SQL = "INSERT INTO modification (type, nomEvent, dateEvent, enseignantEvent, salleEvent, heureDebut, heureFin, groupeEvent)
+			VALUES (1, '".$nom."', '".$date."', '".$enseignant."', '".$salle."', '".$heureDebut."', '".$heureFin."', '".$groupe."')";
+	mysqli_query($conn, $SQL);
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<h1 style="text-align: center;">Demande de création d'un évennement - ENSEIGNANT</h1>
-		<meta charset = 'utf-8'>
-		<link rel='stylesheet' href='page3.css'>
+		<meta name=description content="Demande de création d'un événement">
 	</head>
-	<div>
-		<form id='data' method='POST'>
-			<div>
-				<div style="float:left; border-spacing: 10px">
+	<form  method='POST' action='/EDT/php/edt_main.php?page=3'>
+	  	<div style="position:relative; border-spacing: 10px">
 
-					<?php
-						echo("<form method='POST'><p> Nom de l'évennement <input name='nom' id='name'/></p></form>");
-						if(isset($_POST["nom"])){
-							$nom = $_POST["nom"];
-						}
-			    	?>
+			<?php
+				echo "<label for='nom'>Nom de l'évènement : </label>";
+				echo "<input type='text' name='nom' id='nom'/>";
+	    	?>
 
-			  	</div>
+	  	</div>
 
-			  	<div style="float:left; border-spacing: 10px">
+	  	<div style="position:relative; border-spacing: 10px">
 
-					<?php
-						echo("<form method='POST'><p> Nom de l'enseignant <input name='enseignant' id='enseignant'/></p></form>");
-						if(isset($_POST['enseignant'])){
-							$enseignant = $_POST["enseignant"];
-						}
-			    	?>
+			<?php
+				echo "<label for='enseignant'>Nom de l'enseignant : </label>";
+				echo "<input type='text' name='enseignant' id='enseignant'/>";
+	    	?>
 
-			  	</div>
+	  	</div>
 
-			  	<div style="float:left; border-spacing: 10px">
+	  	<div style="position:relative; border-spacing: 10px">
 
-					<?php
-						echo("<form method='POST'><p> Groupe(s) concerné(s) <input name='groupe' id='groupe'/></p></form>");
-						if(isset($_POST['groupe'])){
-							$groupe = $_POST["groupe"];
-						}
-			    	?>
+			<?php
+				echo "<label for='groupe'>Groupe(s) concerné(s) : </label>";
+				echo "<input type='text' name='groupe' id='groupe'/>";
+	    	?>
 
-			  	</div>
+	  	</div>
 
-				<div style="float:left; border-spacing: 10px">
+	  	<div style="position:relative; border-spacing: 10px">
 
-					<?php
-						echo("<form method='POST'><p> Date <input type='date' name='date' id='date'/></p></form>");
-						if(isset($_POST["date"])){
-							$date = $_POST["date"];
-						}
-						// date_create_from_format ( "d/m/Y" , $date [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
-			    	?>
+			<?php
+				echo "<label for='date'>Date : </label>";
+				echo "<input type='date' name='date' id='date'/>";
 
-			  	</div>
+				// date_create_from_format ( "d/m/Y" , $date [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
+	    	?>
 
-				<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    	Nom de la salle
-			  	</button>
-				<div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="float:left; border-spacing: 10px">
+	  	</div>
 
-					<?php
-			    	while($data_sale = mysqli_fetch_array($requete_salle)){
-			    		if(isset($_POST["salle"])){
-							$salle = $data_salle['salle'];
-			    		}
-						echo("<button class = 'dropdownitem' type = 'submit'> ".$salle." </button>");
-					}
-			    	?>
+	  	<label for="salle">Nom de la salle : </label>
+		<select class="form-control" id="salle" name="salle">
+			<?php
+			foreach($requete_salle as $salle){
+				echo "<option  value='".$salle['nom']."'>".$salle['nom']."</option>";
+			}
+	    	?>
+		</select>
 
-			  	</div>
+	  	<div style="position:relative; border-spacing: 10px">
 
-			  	<div style="float:left; border-spacing: 10px">
+			<?php
+				echo "<label for='hd'>Heure de début : </label>";
+				echo "<input type='time' name='hd' id='hd'/>";
 
-					<?php
-						echo('<form method="POST"><p> Heure de début <input type="time" name="hd"/></p></form>');
-						if(isset($_POST['hd'])){
-							$heureDebut = $_POST["hd"];
-						}
-						// date_create_from_format ( "H:i" , $heureDebut [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
-			    	?>
+				// date_create_from_format ( "H:i" , $heureDebut [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
+	    	?>
 
-			  	</div>
+	  	</div>
 
-				<div style="float:left; border-spacing: 10px">
+	  	<div style="position:relative; border-spacing: 10px">
+			<?php
+				echo "<label for='hf'>Heure de fin : </label>";
+				echo "<input type='time' name='hf' id='hf'/>";
+				// date_create_from_format ( "H:i" , $heureFin [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
+	    	?>
+	  	</div>
 
-					<?php
-						echo("<form method='POST'><p> Heure de fin <input type='time' name='hf'/></p></form>");
-						if(isset($_POST["hf"])){
-							$heureFin = $_POST["hf"];
-						}
-						// date_create_from_format ( "H:i" , $heureFin [, DateTimeZone "e" ] ) : DateTime // Pas sur de cette conversion
-			    	?>
-
-			  	</div>
-
-			</div>
-			<p><input name = "submit" type="submit" value="Valider" style="float: right; width: 25%;"></p>
-		</form>
-	</div>
+		<input name = "submit" type="submit" value="Valider" style="float: right; width: 25%;"></input>
+	</form>
 </html>
